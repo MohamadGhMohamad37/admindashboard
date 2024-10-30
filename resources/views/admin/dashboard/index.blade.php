e78cba37a53be7f4b002e5a2e091e6898b1f8666
MohamadSy37 <107316607+MohamadSy37@users.noreply.github.com>
2024-10-30 23:41:16 +0300

    Admin control panel created

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -1,11 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>admin</h1>
-</body>
-</html>
+@extends('admin.layout.app')
+@section('title','Mohamad Ghazi Mohamad Admin Dashboard')
+@section('header')
+@endsection
+@section('content')
+
+<div class="container">
+          <div class="page-inner">
+            <div
+              class="d-flex align-items-left align-items-md-center flex-column flex-md-row pt-2 pb-4"
+            >
+              <div>
+                <h3 class="fw-bold mb-3">Dashboard</h3>
+                <h6 class="op-7 mb-2">Admin Dashboard Create By Mohamad Ghazi Mohamad</h6>
+              </div>
+            </div>
+          </div>
+        </div>
+        @endsection
+        @section('script')
+        @endsection
