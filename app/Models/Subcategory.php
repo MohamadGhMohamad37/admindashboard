22c1e41c03a23b12277631318ed0c81526434ea5
MohamadSy37 <107316607+MohamadSy37@users.noreply.github.com>
2024-11-01 16:40:57 +0300

    Build a sub-category model and create a relationship between the sub-category and the category

--- app/Models/Subcategory.php
+++ app/Models/Subcategory.php
@@ -5,33 +5,34 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
+class Subcategory extends Model
 {
     use HasFactory;
     protected $fillable = [
         'name_en',
         'name_de',
-        'name_fr',
-        'name_ar',
-        'name_zh',
         'name_tr',
+        'name_ar',
+        'name_fr',
+        'name_zh',
         'description_en',
         'description_de',
-        'description_fr',
-        'description_ar',
-        'description_zh',
         'description_tr',
+        'description_ar',
+        'description_fr',
+        'description_zh',
         'image',
-        'images'
+        'images',
+        'pdf_file',
+        'category_id'
     ];
 
     protected $casts = [
         'images' => 'array',
     ];
-    // Relationship with Subcategory
-    public function subcategories()
+    // Relationship with Category
+    public function category()
     {
-        return $this->hasMany(Subcategory::class);
+        return $this->belongsTo(Category::class);
     }
-
 }
